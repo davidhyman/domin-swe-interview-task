03dc9de5f94f9fcab2b97d0fa94a5606da1ed15c
David Hyman <5198267+davidhyman@users.noreply.github.com>
2024-06-26 16:38:31 +0100

    think about single value analysers

--- processor/demo.py
+++ processor/demo.py
@@ -5,22 +5,65 @@ from typing import Tuple
 Events = List[Tuple[str, float, float]]
 
 
-class Analyser:
-    def analyse(self, arr: numpy.ndarray) -> Events:
+class StreamAnalyser:
+    """potentially stateful analysers that run from the start to the end of the stream"""
+    def analyse(self, timestamp: float, current_value: float) -> bool|float|int:
         pass
 
 
-class WindowRepeatAnalyser:
-    """example of operation over window. some algorithms don't require a full window
-    such as this specific run-length counter, or averaging; a different group of Analysers could be implemented
-    in such cases for greater performance (as they wouldn't repeatedly loop already-processed data)
+class StreamMetric(StreamAnalyser):
+    def analyse(self, timestamp: float, current_value: float) -> float|int:
+        pass
+
+
+class StreamEvent(StreamAnalyser):
+    def analyse(self, timestamp: float, current_value: float) -> bool:
+        pass
+
+
+class Average(StreamMetric):
+    """example of operation over window.
+    different algorithms might want to retain the same data over a window; this could be inefficient
+    in terms of maintaining multiple buffers.
     """
-    def __init__(self, event_when_sequential: int):
-        self.event_when_sequential = event_when_sequential
+    def __init__(self, window_length: int=10):
+        pass
 
-    def analyse(self, arr: numpy.ndarray) -> Events:
-        avg_value = numpy.average(arr)
+    def analyse(self, timestamp: float, current_value: float) -> float:
+        raise NotImplementedError()
 
+
+class RunLength(StreamMetric):
+    """returns the current count of identical values
+    """
+    def __init__(self):
+        self.previous = None
+        self.count = 0
+
+    def analyse(self, timestamp: float, current_value: float) -> int:
+        if self.previous == current_value:
+            self.count += 1
+        else:
+            self.count = 0
+        self.previous = current_value
+        return self.count
+
+
+class GreaterThanEdge(StreamEvent):
+    """returns an event when the new value transitions from being less than (or equal), to greater than
+    the configured value
+    """
+    def __init__(self, value: float=0.5):
+        self.previous = None
+        self.value: float = value
+
+    def analyse(self, timestamp: float, current_value: float) -> bool:
+        outcome = False
+        if self.previous is not None:
+            if (current_value > self.value) and (self.previous <= self.value):
+                outcome = True
+        self.previous = current_value
+        return outcome
 
 
 def run():
@@ -31,10 +74,8 @@ def run():
     channel_processor = numpy.ndarray((1, processor_length))
     current_insertion_index = 0
 
-    analysers = {
-        "detector_1": WindowAvgAnalyser(event_when_greater_than=0.5),
-    }
-
+    gt = GreaterThanEdge(value=0.7)
+    raw_rle = RunLength()
 
 
 __name__ == "__main__" and run()
