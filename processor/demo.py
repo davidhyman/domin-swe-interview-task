03100fa56ba31728f400afd0265d496c454fd16f
David Hyman <5198267+davidhyman@users.noreply.github.com>
2024-06-26 17:35:56 +0100

    output with generators

--- processor/demo.py
+++ processor/demo.py
@@ -1,81 +1,77 @@
+import itertools
+from pathlib import Path
+
 import numpy
-from typing import List
+from typing import List, Generator, Iterable
 from typing import Tuple
 
 Events = List[Tuple[str, float, float]]
 
-
-class StreamAnalyser:
-    """potentially stateful analysers that run from the start to the end of the stream"""
-    def analyse(self, timestamp: float, current_value: float) -> bool|float|int:
-        pass
+OUTFILE = Path("./events.csv")
 
 
-class StreamMetric(StreamAnalyser):
-    def analyse(self, timestamp: float, current_value: float) -> float|int:
-        pass
+def gen_run_length(data: Iterable[float|int]) -> Generator[int, None, None]:
+    """returns the current count of identical values"""
+    previous = None
+    count: int = 0
 
-
-class StreamEvent(StreamAnalyser):
-    def analyse(self, timestamp: float, current_value: float) -> bool:
-        pass
-
-
-class Average(StreamMetric):
-    """example of operation over window.
-    different algorithms might want to retain the same data over a window; this could be inefficient
-    in terms of maintaining multiple buffers.
-    """
-    def __init__(self, window_length: int=10):
-        pass
-
-    def analyse(self, timestamp: float, current_value: float) -> float:
-        raise NotImplementedError()
-
-
-class RunLength(StreamMetric):
-    """returns the current count of identical values
-    """
-    def __init__(self):
-        self.previous = None
-        self.count = 0
-
-    def analyse(self, timestamp: float, current_value: float) -> int:
-        if self.previous == current_value:
-            self.count += 1
+    for value in data:
+        if previous == value:
+            count += 1
         else:
-            self.count = 0
-        self.previous = current_value
-        return self.count
+            count = 0
+        previous = value
+        yield count
 
 
-class GreaterThanEdge(StreamEvent):
+def gen_rising_edge(data: Iterable[float|int], threshold: float) -> Generator[bool, None, None]:
     """returns an event when the new value transitions from being less than (or equal), to greater than
-    the configured value
+    the configured threshold
     """
-    def __init__(self, value: float=0.5):
-        self.previous = None
-        self.value: float = value
+    previous = None
 
-    def analyse(self, timestamp: float, current_value: float) -> bool:
+    for value in data:
         outcome = False
-        if self.previous is not None:
-            if (current_value > self.value) and (self.previous <= self.value):
+        if previous is not None:
+            if (value > threshold) and (previous <= threshold):
                 outcome = True
-        self.previous = current_value
-        return outcome
+        previous = value
+        yield outcome
+
+
+def output_event(name: str, timestamp: float, event: bool) -> None:
+    if not event:
+        return
+    with OUTFILE.open("a") as fh:
+        fh.write(f"{timestamp},{name}\n")
 
 
 def run():
     data = [0] * 1000 + [1] * 1000 + [0] * 1000
 
-    max_window_ms = 500
-    processor_length = max_window_ms * 2
-    channel_processor = numpy.ndarray((1, processor_length))
-    current_insertion_index = 0
+    # working with generators allows us to compose processing pipelines in a functional style
 
-    gt = GreaterThanEdge(value=0.7)
-    raw_rle = RunLength()
+    # we split our source data before handing it to individual processing pipelines
+    # so that they can increment at their own pace
+    # the pipelines themselves could be multiprocessed, in the case that the processing is more costly
+    # than the data transfer
+    raw_data, input_gt, input_gen_rising_edge = itertools.tee(data, 3)
+
+    # since we can't reuse generators, we'd have to do something a bit fancier if we wanted
+    # to store intermediate results, such as "analyse_runlength"
+    analyse_rising_edge = gen_rising_edge(input_gt, threshold=0.7)
+    analyse_runlength = gen_run_length(input_gen_rising_edge)
+    analyse_rising_edge_of_runlength = gen_rising_edge(analyse_runlength, 9)
+
+    counter = 0
+    for value in raw_data:
+        counter += 1
+
+        event = next(analyse_rising_edge_of_runlength)
+        output_event("ten_identical_values", counter, event)
+
+        event = next(analyse_rising_edge)
+        output_event("more_than_0.7", counter, event)
 
 
 __name__ == "__main__" and run()
